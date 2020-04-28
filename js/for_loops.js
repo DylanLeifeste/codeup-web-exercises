f9bb896560c76286e15f626862c1633b6abd5f16
Dylan Leifeste <ldylan01@gmail.com>
2020-04-28 15:53:18 -0500

    loops in js

---
+++ js/for_loops.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+// function showMulitplicationTable() {
+//     var base = prompt("Select a number");
+//     var num = 1;
+//
+//     for (num; num <= 10; ++num)
+//     console.log(base + " * " + num + " = " + base * num);
+//
+// }
+//  console.log(showMulitplicationTable());
+
+
+// for (var i = 1; i <= 10; i++) {
+//
+//     var x = Math.floor(Math.random() * 180) + 20;
+//
+//     if (x % 2 === 0) {
+//         console.log(x + " Is a even number");
+//     } else {
+//         console.log(x + " Is a odd number");
+//     }
+//
+// }
+
+// for (var i = 1; i <= 9; i++) {
+//     var x = "".repeat(9 - i) + (i + "").repeat(i);
+//     console.log(x)
+// }
+
+for (var i = 100; i >= 5; i -=5) {
+    console.log(i)
+}
+
